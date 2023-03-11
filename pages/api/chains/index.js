9875db433e0eb54697cabbef9cc80d9534e9151b
Jon Miles <jonathandanielmiles@gmail.com>
2023-03-11 09:25:29 +0000

    Inject original path to chain icon, better usability

--- pages/api/chains/index.js
+++ pages/api/chains/index.js
@@ -1,9 +1,17 @@
+import absoluteUrl from "next-absolute-url";
+
 import { chains } from "../../../data/chains";
 
 export default async function handler(req, res) {
   res.setHeader("Cache-Control", "s-maxage=3600, stale-while-revalidate");
 
+  const { origin } = absoluteUrl(req);
+  const chainsInjectPaths = chains.map((chain) => ({
+    ...chain,
+    icon: `${origin}${chain.icon}`,
+  }));
+
   if (req.method === "GET") {
-    return res.status(200).json(chains);
+    return res.status(200).json(chainsInjectPaths);
   }
 }
