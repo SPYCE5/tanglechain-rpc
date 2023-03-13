00b17497bc2a57d247c45da17385ccea12bb76db
Developer-555 <127768150+Developer-555@users.noreply.github.com>
2023-03-13 16:22:39 +0100

    Tutorial

--- data/chains.js
+++ data/chains.js
@@ -27,4 +27,33 @@ export const chains = [
       },
     ],
   },
+  
+    {
+    name: "Shimmer EVM Testnet2",
+    chain: "SMR",
+    icon: "/icons/shimmer.svg",
+    rpc: ["https://json-rpc.evm.testnet.shimmer.network"],
+    features: [],
+    faucets: [],
+    nativeCurrency: {
+      name: "Shimmer",
+      symbol: "SMR",
+      decimals: 18,
+    },
+    infoURL: "https://shimmer.network/",
+    shortName: "Shimmer",
+    chainId: 1070,
+    networkId: 1070,
+    slip44: 60,
+    explorers: [
+      {
+        name: "BlockScout",
+        url: "https://explorer.evm.testnet.shimmer.network",
+        standard: "EIP3091",
+      },
+    ],
+  },
+  
+  
+  
 ];
