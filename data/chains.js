cf335d446b0ad4120ac5685c0ba774a4b1a55b84
Tim Sigl <33939264+Schereo@users.noreply.github.com>
2023-10-02 09:15:02 +0200

    Add ShimmerEVM
    - Adds ShimmerEVM to TangleChains

--- data/chains.js
+++ data/chains.js
@@ -24,7 +24,29 @@ export const chains = [
       },
     ],
   },
-   
- 
-  
+  {
+    name: "ShimmerEVM",
+    chain: "SMR",
+    icon: "/icons/shimmer.svg",
+    rpc: ["https://json-rpc.evm.shimmer.network/"],
+    features: [],
+    faucets: [],
+    nativeCurrency: {
+      name: "Shimmer",
+      symbol: "SMR",
+      decimals: 6,
+    },
+    infoURL: "https://shimmer.network/",
+    shortName: "Shimmer",
+    chainId: 148,
+    networkId: 148,
+    slip44: 4219,
+    explorers: [
+      {
+        name: "Explorer",
+        url: "https://explorer.evm.shimmer.network/",
+        standard: "EIP3091",
+      },
+    ],
+  },
 ];
